f484a7a6c3a815bfe4e9b2bd3c7ab09ddc04f734
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-04-04 01:14:43 +0200

    complete data to database parsing

--- lab1/parse.py
+++ lab1/parse.py
@@ -8,8 +8,45 @@ import pandas as pd
 
 db_file = 'db/lab1_dev.db'
 swiat_excel = 'raw_data/xls/zagranica.xls'
+swiat_table_name = 'swiat'
 main_excel_dir = 'raw_data/xls/obwody'
 main_index = ['Kod_gminy', 'Nr_obw']
+main_table_name = 'kraj'
+wojewodztwa = {
+    'Kod_wojewodztwa': [
+        '02',
+        '04',
+        '06',
+        '08',
+        '10',
+        '12',
+        '14',
+        '16',
+        '18',
+        '20',
+        '22',
+        '24',
+        '26',
+        '28',
+        '30',
+        '32'],
+    'Nazwa': [
+        'WOJ. DOLNOŚLĄSKIE',
+        'WOJ. KUJAWSKO-POMORSKIE',
+        'WOJ. LUBELSKIE',
+        'WOJ. LUBUSKIE',
+        'WOJ. ŁÓDZKIE',
+        'WOJ. MAŁOPOLSKIE',
+        'WOJ. MAZOWIECKIE',
+        'WOJ. OPOLSKIE',
+        'WOJ. PODKARPACKIE',
+        'WOJ. PODLASKIE',
+        'WOJ. POMORSKIE',
+        'WOJ. ŚLĄSKIE',
+        'WOJ. ŚWIĘTOKRZYSKIE',
+        'WOJ. WARMIŃSKO-MAZURSKIE',
+        'WOJ. WIELKOPOLSKIE',
+        'WOJ. ZACHODNIOPOMORSKIE']}
 
 
 # SCRIPT
@@ -31,7 +68,7 @@ try:
     conn.execute('drop table if exists ogolne')
     conn.execute('drop table if exists swiat')
     conn.execute('drop table if exists kraj')
-    conn.execute('drop table if exists wojewodztwo')
+    conn.execute('drop table if exists wojewodztwa')
     conn.execute('drop table if exists okreg')
     conn.execute('drop table if exists gmina')
     conn.execute('drop table if exists obwod')
@@ -58,7 +95,7 @@ for file in os.listdir(dir):
         df.set_index(main_index, inplace=True)
         # insert to db
         try:
-            df.to_sql('obwod', conn, if_exists='append', index=True)
+            df.to_sql(main_table_name, conn, if_exists='append', index=True)
         except Exception as e:
             print(e)
             exit(1)
@@ -68,12 +105,19 @@ print('swiat...')
 df = pd.read_excel(swiat_excel)
 df.rename(columns=lambda x: re.sub("[\s|.|,]+", "_", x))
 try:
-    df.to_sql('swiat', conn, if_exists='fail', index=True, index_label='id')
+    df.to_sql(swiat_table_name, conn, if_exists='fail', index=True, index_label='id')
 except Exception as e:
     print(e)
     exit(1)
 
-# TODO: kody wojewodztw
+# load wojewodztwa
+df = pd.DataFrame.from_dict(wojewodztwa)
+df.set_index('Kod_wojewodztwa', inplace=True, drop=False)
+try:
+    df.to_sql('wojewodztwa', conn, if_exists='fail', index=True, index_label='id')
+except Exception as e:
+    print(e)
+    exit(1)
 
 # clean up and exit
 print('all data saved to database')
