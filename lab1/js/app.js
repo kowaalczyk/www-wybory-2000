623dee96b64fba340462207adabf23a7c3c2ecee
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-04-03 11:17:19 +0200

    add chart filtering, prepare js for api requests

--- lab1/js/app.js
+++ lab1/js/app.js
@@ -32,39 +32,143 @@ const colors = [
     [255, 159, 64]
 ];
 
-const exampleDatasetsNormal = [
-    860,
-    411,
-    2086,
-    5732,
-    39563,
-    71615,
-    1299,
-    1932,
-    66256,
-    135,
-    2845,
-    250
+const exampleApiResponse = {
+    scope: {
+        name: 'Kraków',
+        type: 'gmina',
+        location: 'powiat Kraków, woj. Małopolskie',
+        href: '/gminy/'
+    },
+    subScope: {
+        type: 'obwód',
+        location: 'gmina Kraków, woj. Małopolskie',
+        href: false
+    },
+    data: {
+        normal: [{
+            label: 'Liczba głosów',
+            data: [
+                860,
+                411,
+                2086,
+                5732,
+                39563,
+                71615,
+                1299,
+                1932,
+                66256,
+                135,
+                2845,
+                250
+            ]
+        }],
+        percent: [{
+            label: 'Procent głosów',
+            data: [
+                0.445632798573975,
+                0.212971023504539,
+                1.08091862537827,
+                2.97019442026282,
+                20.5006632674211,
+                37.1092940347386,
+                0.673112796915807,
+                1.00111926377316,
+                34.3323798864155,
+                0.069953985822659,
+                1.47421547900344,
+                0.129544418190109
+            ]
+        }],
+        filterable: [{
+            label: 'Obwód #1 Kraków',
+            data: [
+                860,
+                411,
+                2086,
+                5732,
+                39563,
+                71615,
+                1299,
+                1932,
+                66256,
+                135,
+                2845,
+                250
+            ],
+            backgroundColor: calculateBackgroundColor(colors[0]),  // TODO: Generate
+            borderColor: calculateBorderColor(colors[0]),  // TODO: Generate
+            borderWidth: 1 // TODO: Default
+        }, {
+            label: 'Obwód #2 Kraków',
+            data: [
+                860,
+                411,
+                2086,
+                5732,
+                39563,
+                71615,
+                1299,
+                1932,
+                66256,
+                135,
+                2845,
+                250
+            ],
+            backgroundColor: calculateBackgroundColor(colors[1]),  // TODO: Generate
+            borderColor: calculateBorderColor(colors[1]),  // TODO: Generate
+            borderWidth: 1 // TODO: Default
+        }]
+    }
+};
+
+const menuListPrefix = [
+    {
+        position: 0,
+        text: 'Wyniki zbiorcze',
+        link: '/'
+    },
+    {
+        position: 1,
+        text: 'Wyniki krajowe',
+        link: '/polska/'
+    },
+    {
+        position: 2,
+        text: 'Wyniki zagraniczne',
+        link: '/zagraniczne/'
+    }
 ];
 
-const exampleDatasetsPercent = [
-    0.445632798573975,
-    0.212971023504539,
-    1.08091862537827,
-    2.97019442026282,
-    20.5006632674211,
-    37.1092940347386,
-    0.673112796915807,
-    1.00111926377316,
-    34.3323798864155,
-    0.069953985822659,
-    1.47421547900344,
-    0.129544418190109
+const menuListSuffix = [
+    {
+        position: 99,
+        text: 'O aplikacji',
+        link: '/info/'
+    }
 ];
 
-const exampleDatasetsFilterable = [
+const createMenuLinks = apiResponse => {
+    let ans = [];
+    menuLinksPrefix.forEach(i => ans.push(i));
 
-];
+    if(apiResponse.scope.href) {
+        ans.push({
+            position: ans.size(),
+            text: apiResponse.scope.type,
+            link: apiResponse.scope.href
+        });
+    }
+    if(apiResponse.subScope.href) {
+        ans.push({
+            position: ans.size(),
+            text: apiResponse.subScope.type,
+            link: apiResponse.subScope.href
+        });
+    }
+
+    menuLinksSuffix.forEach(i => ans.push(i));
+    return ans;
+};
 
 Chart.defaults.global.defaultFontSize = 12;
 Chart.defaults.global.defaultFontColor = '#202020';
@@ -74,7 +178,7 @@ Chart.defaults.global.defaultFontFamily = "'Roboto Mono', monospace";
 Vue.component('main-chart', {
     extends: VueChartJs.Bar,
     mixins: [VueChartJs.mixins.reactiveProp],
-    props: ['options'],
+    props: ['chartData', 'options'],
     mounted() {
         this.renderChart(this.chartData, this.options);
     }
@@ -84,67 +188,56 @@ Vue.component('main-chart', {
 let app = new Vue({
     el: '#vue-app',
     data: {
-        scope: {
-            name: 'Kraków',
-            type: 'gmina',
-            location: 'powiat Kraków, woj. Małopolskie'
-        },
-        chartData: {
-            labels: candidateNameLabels,
-            datasets: [{
-                label: 'Liczba głosów',
-                data: exampleDatasetsNormal,
-                backgroundColor: calculateBackgroundColor(colors[0]),
-                borderColor: calculateBorderColor(colors[0]),
-                borderWidth: 1
-            }, {
-                label: 'Liczba głosów 2',
-                data: exampleDatasetsNormal,
-                backgroundColor: calculateBackgroundColor(colors[1]),
-                borderColor: calculateBorderColor(colors[1]),
-                borderWidth: 1
-            }]
-        },
-        chartOptions: {
-            animation: false,
-            responsive: true,
-            maintainAspectRatio: false,
-            legend: {
-                display: true,
-                position: 'bottom'
-            },
-            scales: {
-                xAxes: [{
-                    type: 'category',
-                    ticks: {
-                        autoSkip: false,
-                        maxRotation: 90,
-                        minRotation: 90,
+        currentApiResponse: exampleApiResponse,
+        currentVisibleData: {
+            scope: exampleApiResponse.scope,  // TODO: Function for loading
+            chart: {
+                data: {
+                    labels: candidateNameLabels,
+                    datasets: exampleApiResponse.data.filterable
+                },
+                options: {
+                    animation: false,
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    legend: {
+                        display: true,
+                        position: 'bottom'
                     },
-                    stacked: true
-                }],
-                yAxes: [{
-                    ticks: {
-                        display: false,
-                        beginAtZero: true
+                    scales: {
+                        xAxes: [{
+                            type: 'category',
+                            ticks: {
+                                autoSkip: false,
+                                maxRotation: 90,
+                                minRotation: 90,
+                            },
+                            stacked: true
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                display: false,
+                                beginAtZero: true
+                            },
+                            stacked: true
+                        }]
                     },
-                    stacked: true
-                }]
-            },
-            layout: {
-                padding: {
-                    left: 12,
-                    right: 12,
-                    top: 12,
-                    bottom: 12
+                    layout: {
+                        padding: {
+                            left: 12,
+                            right: 12,
+                            top: 12,
+                            bottom: 12
+                        }
+                    },
+                    title: {
+                        display: true,
+                        position: 'bottom',
+                        fontFamily: Chart.defaults.global.defaultFontFamily,
+                        fontStyle: 'normal',
+                        text: 'Kliknij na legendzie aby filtrować wyniki'
+                    }
                 }
-            },
-            title: {
-                display: true,
-                position: 'bottom',
-                fontFamily: Chart.defaults.global.defaultFontFamily,
-                fontStyle: 'normal',
-                text: 'Kliknij na legendzie aby filtrować wyniki'
             }
         },
         expandables: {
@@ -168,15 +261,31 @@ let app = new Vue({
                 this.closeExpandables();
             }
             this.expandables[expandableName] = !prevState;
+
+            // TODO: Remove
+            console.log(createMenuLinks(exampleApiResponse));
         },
-        setScorePct() {
-            // TODO
-        },
-        setScoreCount() {
-            // TODO
-        },
-        setScoreFilterable() {
-            // TODO
+        setScoresType(event, type) {
+            console.log('setting data as visible: ', type);
+
+            // change datasets for chart
+            event.preventDefault();
+            let currentData = this.currentVisibleData.chart.data;
+            currentData.datasets = this.currentApiResponse.data[type];
+            this.currentVisibleData.chart.data = currentData;
+            // TODO: Force re-render, currently need to click chart
+
+            // additional tweaks
+            switch (type) {
+                case 'normal':
+                    break;
+                case 'percent':
+                    break;
+                case 'filterable':
+                    break;
+                default:
+                    break;
+            }
         }
     }
 });
